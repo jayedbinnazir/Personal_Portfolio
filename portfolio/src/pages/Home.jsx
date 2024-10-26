4702a130a9ec329dc21913e6425086846244d02a
jayedbinnazir <jayed.freelance@gmail.com>
2024-10-26 14:09:31 +0600

    loading

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -26,6 +26,10 @@ const Home = () => {
     getMyProfile();
   }, []);
 
+  if (!user) {
+    return <h1>...Loading</h1>;
+  }
+
   return (
     <article className="px-5 mt-10 sm:mt-14 md:mt-16 lg:mt-24 xl:mt-32 sm:mx-auto w-full max-w-[1050px] flex flex-col gap-14">
       <ProfileContext.Provider value={user}>
